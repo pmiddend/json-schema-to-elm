b658917423113d5708854a545add64ba215fff12
Philipp Middendorf <philipp.middendorf@desy.de>
2023-04-21 10:23:35 +0200

    ✨ Parse first JSON schema, resolve definitions

---
+++ app/JsonSchema.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module JsonSchema where
+
+import Control.Applicative (pure)
+import Control.Monad (fail)
+import Data.Aeson (FromJSON (parseJSON), withObject, (.:), (.:?))
+import Data.Either (Either (Left, Right))
+import Data.Fix (Fix, wrapFix)
+import Data.Foldable (Foldable)
+import Data.Function (($))
+import Data.Functor (Functor, (<$>))
+import qualified Data.Map as Map
+import Data.Maybe (Maybe (Just, Nothing))
+import Data.Semigroup ((<>))
+import Data.Text (Text, breakOnEnd, isPrefixOf, unpack)
+import Data.Traversable (Traversable, traverse)
+import Text.Show (Show)
+import Prelude (snd)
+
+data SubschemaTypeF a
+  = SubschemaTypeObject {properties :: Map.Map Text a}
+  | SubschemaTypeEnum {enumMembers :: [Text]}
+  | SubschemaTypeInteger
+  | SubschemaTypeString
+  | SubschemaTypeNumber
+  deriving (Show, Foldable, Traversable, Functor)
+
+data RefOrSubschema
+  = RefOrSubschemaRef Text
+  | RefOrSubschemaSchema (SubschemaF RefOrSubschema)
+  deriving (Show)
+
+data SubschemaF a = SubschemaF
+  { title :: Text,
+    required :: Maybe [Text],
+    type_ :: SubschemaTypeF a,
+    definitions :: Maybe (Map.Map Text a)
+  }
+  deriving (Show, Foldable, Functor, Traversable)
+
+type Subschema = Fix SubschemaF
+
+instance FromJSON RefOrSubschema where
+  parseJSON originalValue = withObject "RefOrSubschema" processor originalValue
+    where
+      processor v = do
+        ref <- v .:? "$ref"
+        case ref of
+          Nothing -> RefOrSubschemaSchema <$> parseJSON originalValue
+          Just ref' -> pure (RefOrSubschemaRef ref')
+
+instance FromJSON (SubschemaF RefOrSubschema) where
+  parseJSON = withObject "Subschema" $ \v -> do
+    title' <- v .: "title"
+    type' :: Maybe Text <- v .:? "type"
+    required' <- v .:? "required"
+    definitions' <- v .:? "definitions"
+    resolvedType <- case type' of
+      Just "integer" -> pure SubschemaTypeInteger
+      Just "string" -> pure SubschemaTypeString
+      Just "number" -> pure SubschemaTypeNumber
+      Just "object" -> do
+        properties' <- v .: "properties"
+        subProperties <- traverse parseJSON properties'
+        pure (SubschemaTypeObject subProperties)
+      Just unknownType -> fail ("unknown schema type \"" <> unpack unknownType <> "\"")
+      Nothing -> SubschemaTypeEnum <$> (v .: "enum")
+
+    pure (SubschemaF title' required' resolvedType definitions')
+
+resolveRefs :: SubschemaF RefOrSubschema -> Either Text Subschema
+resolveRefs s =
+  case resolvedDefs of
+    Left e -> Left e
+    Right resolvedDefs' -> wrapFix <$> traverse (resolveRef resolvedDefs') s
+  where
+    resolvedDefs :: Either Text (Map.Map Text Subschema)
+    resolvedDefs = case definitions s of
+      Nothing -> Right Map.empty
+      Just defs -> traverse (resolveRef Map.empty) defs
+    resolveRef :: Map.Map Text Subschema -> RefOrSubschema -> Either Text Subschema
+    resolveRef _ (RefOrSubschemaSchema x) = resolveRefs x
+    resolveRef defs (RefOrSubschemaRef ref) =
+      if "#/definitions/" `isPrefixOf` ref
+        then case Map.lookup (snd (breakOnEnd "/" ref)) defs of
+          Nothing -> Left ("ref " <> ref <> ": not found in definitions in root")
+          Just v -> Right v
+        else Left ("ref " <> ref <> ": doesn't start with \"#/definitions\"")
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad (forM_)
+import Data.Aeson (eitherDecodeStrict)
+import Data.ByteString (getContents)
+import Data.Either (Either (Left, Right))
+import Data.Fix (Fix (..))
+import Data.Function (($))
+import Data.Int (Int)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.Semigroup ((<>))
+import Data.String (String)
+import Data.Text (intercalate, pack, replicate)
+import Data.Text.IO (putStrLn)
+import JsonSchema (RefOrSubschema, Subschema, SubschemaF (required, title, type_), SubschemaTypeF (SubschemaTypeEnum, SubschemaTypeInteger, SubschemaTypeNumber, SubschemaTypeObject, SubschemaTypeString, enumMembers), resolveRefs)
+import System.IO (IO)
+import Prelude (Num ((+)))
+
+printSubschema :: Subschema -> IO ()
+printSubschema = printSubschema' 0
+  where
+    printSubschema' :: Int -> Subschema -> IO ()
+    printSubschema' indent (Fix ss) = do
+      putStrLn (replicate indent " " <> title ss <> ", required: " <> intercalate ", " (fromMaybe [] (required ss)) <> ", type: ")
+      printSubschemaType indent (type_ ss)
+    printSubschemaType :: Int -> SubschemaTypeF Subschema -> IO ()
+    printSubschemaType indent SubschemaTypeInteger = putStrLn (replicate indent " " <> "integer")
+    printSubschemaType indent SubschemaTypeString = putStrLn (replicate indent " " <> "string")
+    printSubschemaType indent SubschemaTypeNumber = putStrLn (replicate indent " " <> "number")
+    printSubschemaType indent (SubschemaTypeEnum {enumMembers = m}) = putStrLn (replicate indent " " <> "enum(" <> intercalate "," m <> ")")
+    printSubschemaType indent (SubschemaTypeObject props) = do
+      putStrLn (replicate indent " " <> "object, props:")
+      forM_ (Map.toList props) $ \(name, type_') -> do
+        putStrLn (replicate (indent + 1) " " <> name)
+        printSubschema' (indent + 2) type_'
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  stdin <- getContents
+  case eitherDecodeStrict stdin :: Either String (SubschemaF RefOrSubschema) of
+    Left e -> putStrLn (pack e)
+    Right v -> case resolveRefs v of
+      Left e' -> putStrLn e'
+      Right resolved -> printSubschema resolved
