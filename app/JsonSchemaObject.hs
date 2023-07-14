6f7463c1ee1961883335ec4af057f21da5589806
Philipp Middendorf <philipp.middendorf@desy.de>
2023-07-14 10:38:30 +0200

    Support unions

--- app/JsonSchemaObject.hs
+++ app/JsonSchemaObject.hs
@@ -13,9 +13,9 @@ import Data.Functor ((<$>))
 import qualified Data.Map as Map
 import Data.Maybe (Maybe (Just, Nothing), fromMaybe)
 import Data.Monoid (Monoid (..))
-import Data.Ord (Ord)
 import Data.Semigroup ((<>))
-import Data.Text (Text, drop, isPrefixOf, length, unpack)
+import Data.Text (Text, drop, isPrefixOf, length)
+import Data.Traversable (Traversable (traverse))
 import GHC.Generics (Generic)
 import qualified JsonSchemaObjectRef as Ref
 import JsonSchemaTypeEnum
@@ -41,34 +41,41 @@ instance FromJSON JsonSchemaObject
 expectedRefPrefix :: Text
 expectedRefPrefix = "#/definitions/"
 
-resolveMap :: (Foldable t, Ord k) => Ref.JsonSchemaObjectRef -> t (k, Ref.JsonSchemaObjectRef) -> Either Text (Map.Map k JsonSchemaObject)
-resolveMap root =
+resolveMap :: (Foldable t) => ObjectMap -> t (Text, Ref.JsonSchemaObjectRef) -> Either Text ObjectMap
+resolveMap rootDefs =
   foldM
-    (\prevMap (newKey, newElement) -> (\resolvedEl -> Map.insert newKey resolvedEl prevMap) <$> resolveSingleDefinition (fromMaybe mempty (Ref.definitions root)) newElement)
+    (\prevMap (newKey, newElement) -> (\resolvedEl -> Map.insert newKey resolvedEl prevMap) <$> resolveSingleDefinition rootDefs newElement)
     mempty
 
-resolveSingleDefinition :: Ref.ObjectRefMap -> Ref.JsonSchemaObjectRef -> Either Text JsonSchemaObject
-resolveSingleDefinition refdefs root =
+resolveSingleDefinition :: ObjectMap -> Ref.JsonSchemaObjectRef -> Either Text JsonSchemaObject
+resolveSingleDefinition rootDefs root =
   case Ref.ref root of
+    -- The currently looked-at node is a ref itself (shouldn't apply to the root node)
     Just ref' ->
       if not (expectedRefPrefix `isPrefixOf` ref')
         then Left ("reference doesn't start with \"" <> expectedRefPrefix <> "\": " <> ref')
-        else case Map.lookup (drop (length expectedRefPrefix) ref') refdefs of
-          Nothing -> Left ("looking up #/definitions/" <> ref' <> ": not found")
-          Just result -> resolveSingleDefinition refdefs result
+        else case Map.lookup (drop (length expectedRefPrefix) ref') rootDefs of
+          Nothing -> Left ("looking up " <> ref' <> ": not found")
+          Just result -> Right result
+    -- The currently looked-at node is not a ref
     Nothing -> do
-      defs <- resolveMap root (Map.toList (fromMaybe mempty (Ref.definitions root)))
+      defs <- case Ref.definitions root of
+        Nothing -> Right mempty
+        Just defs' -> resolveMap rootDefs (Map.toList defs')
+
+      -- Next, look at the properties of the object. They also need to be resolved because they might contain refs.
       props <- case Ref.properties root of
         Nothing -> Right Nothing
         Just props' ->
-          Just <$> resolveMap root (Map.toList props')
+          Just <$> resolveMap rootDefs (Map.toList props')
 
+      -- Same game for the anyOf alternatives
       anyOfs <- case Ref.anyOf root of
         Nothing -> Right Nothing
         Just anyOfs' ->
           Just
             <$> foldM
-              (\prevList newElement -> (: prevList) <$> resolveSingleDefinition (fromMaybe mempty (Ref.definitions root)) newElement)
+              (\prevList newElement -> (: prevList) <$> resolveSingleDefinition rootDefs newElement)
               mempty
               anyOfs'
       pure
@@ -83,5 +90,58 @@ resolveSingleDefinition refdefs root =
             anyOfs
         )
 
+-- Map k v -> (v -> m b) -> Map k (m b) -> m (Map k b)
+resolveRootDefinitions :: Ref.ObjectRefMap -> Either Text ObjectMap
+resolveRootDefinitions rootDefs =
+  let resolveMap' :: (Foldable t) => t (Text, Ref.JsonSchemaObjectRef) -> Either Text ObjectMap
+      resolveMap' =
+        foldM
+          (\prevMap (newKey, newElement) -> (\resolvedEl -> Map.insert newKey resolvedEl prevMap) <$> resolveRootDefinition newElement)
+          mempty
+
+      resolveRootDefinition :: Ref.JsonSchemaObjectRef -> Either Text JsonSchemaObject
+      resolveRootDefinition root = case Ref.ref root of
+        Just ref' ->
+          if not (expectedRefPrefix `isPrefixOf` ref')
+            then Left ("reference doesn't start with \"" <> expectedRefPrefix <> "\": " <> ref')
+            else case Map.lookup (drop (length expectedRefPrefix) ref') rootDefs of
+              Nothing -> Left ("looking up " <> ref' <> ": not found")
+              Just result -> resolveRootDefinition result
+        Nothing -> do
+          -- Next, look at the properties of the object. They also need to be resolved because they might contain refs.
+          props <- case Ref.properties root of
+            Nothing -> Right Nothing
+            Just props' ->
+              Just <$> resolveMap' (Map.toList props')
+
+          -- Same game for the anyOf alternatives
+          anyOfs <- case Ref.anyOf root of
+            Nothing -> Right Nothing
+            Just anyOfs' ->
+              Just
+                <$> foldM
+                  (\prevList newElement -> (: prevList) <$> resolveRootDefinition newElement)
+                  mempty
+                  anyOfs'
+          pure
+            ( JsonSchemaObject
+                (Ref.title root)
+                (Ref.description root)
+                (Ref.type_ root)
+                props
+                (Ref.required root)
+                (Ref.enum root)
+                mempty
+                anyOfs
+            )
+   in traverse resolveRootDefinition rootDefs
+
+-- Here it starts, and we assume we have no prior resolved definitions in our definitions map (see the mempty)
 fromRef :: Ref.JsonSchemaObjectRef -> Either Text JsonSchemaObject
-fromRef = resolveSingleDefinition mempty
+fromRef root =
+  case Ref.ref root of
+    -- The currently looked-at node is a ref itself (shouldn't apply to the root node)
+    Just ref' -> Left ("root element is a ref to " <> ref' <> " - that's not supported!")
+    Nothing -> do
+      rootDefs <- resolveRootDefinitions (fromMaybe mempty (Ref.definitions root))
+      resolveSingleDefinition rootDefs root
--- app/Main.hs
+++ app/Main.hs
@@ -2,61 +2,119 @@
 
 module Main where
 
+import Control.Applicative (Applicative (pure))
+import Control.Monad (foldM)
 import Data.Aeson (eitherDecodeStrict)
 import Data.ByteString (getContents)
 import Data.Either (Either (Left, Right))
--- import Elm (ToElmMode (Deep), jsonSchemaToElm)
--- import JsonSchema (RefOrSubschema, Subschema, SubschemaF (definitions, required, title, type_), SubschemaTypeF (SubschemaTypeArray, SubschemaTypeBoolean, SubschemaTypeEnum, SubschemaTypeInteger, SubschemaTypeNumber, SubschemaTypeObject, SubschemaTypeString, enumMembers), resolveRefs)
--- import JsonSchemaNew (SchemaType, SchemaTypeEnum)
-
-import Data.Maybe (Maybe (..))
-import Data.Monoid (Monoid (..))
+import Data.Foldable (Foldable (elem))
+import Data.Function ((.))
+import Data.Functor ((<$>))
+import Data.Int (Int)
+import Data.List (length, take, zipWith)
+import qualified Data.Map as Map
+import Data.Maybe (Maybe (..), fromMaybe)
 import Data.Semigroup ((<>))
 import Data.String (String)
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, singleton)
 import Data.Text.IO (putStrLn)
--- import Elm (ElmType, Union (Union), UnionAlternative (UnionAlternative), unionToElm)
-
-import Elm (Module)
-import JsonSchemaObject (JsonSchemaObject (title, type_), fromRef)
+import Data.Text.Manipulate (toCamel, toPascal)
+import Data.Traversable (Traversable (traverse))
+import Debug.Trace (traceShowId)
+import Elm (Dec, Module, Type, decType, decTypeAlias, importEvery, module_, render, tparam, tparams, trecord, tvar)
+import JsonSchemaObject (JsonSchemaObject (anyOf, definitions, enum, properties, required, title, type_), fromRef)
 import JsonSchemaObjectRef (JsonSchemaObjectRef)
 import JsonSchemaTypeEnum (SchemaTypeEnum (..))
 import System.IO (IO)
 import Text.Pretty.Simple (pPrint)
 import Text.Show (Show (..))
-import Prelude (Maybe (Just), print, undefined)
+import Prelude ()
 
--- printSubschema :: Subschema -> IO ()
--- printSubschema = printSubschema' 0
---   where
---     printSubschema' :: Int -> Subschema -> IO ()
---     printSubschema' indent (Fix ss) = do
---       putStrLn (replicate indent " " <> title ss <> ", required: " <> intercalate ", " (fromMaybe [] (required ss)) <> ", type: ")
---       printSubschemaType indent (type_ ss)
---     printSubschemaType :: Int -> SubschemaTypeF Subschema -> IO ()
---     printSubschemaType indent SubschemaTypeInteger = putStrLn (replicate indent " " <> "integer")
---     printSubschemaType indent SubschemaTypeString = putStrLn (replicate indent " " <> "string")
---     printSubschemaType indent SubschemaTypeNumber = putStrLn (replicate indent " " <> "number")
---     printSubschemaType indent SubschemaTypeBoolean = putStrLn (replicate indent " " <> "boolean")
---     printSubschemaType indent (SubschemaTypeEnum {enumMembers = m}) = putStrLn (replicate indent " " <> "enum(" <> intercalate "," m <> ")")
---     printSubschemaType indent (SubschemaTypeArray items) = do
---       putStrLn (replicate indent " " <> "array, subtype:")
---       printSubschema' (indent + 2) items
---     printSubschemaType indent (SubschemaTypeObject props) = do
---       putStrLn (replicate indent " " <> "object, props:")
---       forM_ (Map.toList props) $ \(name, type_') -> do
---         putStrLn (replicate (indent + 1) " " <> name)
---         printSubschema' (indent + 2) type_'
+packShow :: Show a => a -> Text
+packShow = pack . show
 
-schemaToElm :: JsonSchemaObject -> Either Text Module
-schemaToElm j = case type_ j of
+schemaTypeToElmShallow :: JsonSchemaObject -> Either Text Type
+schemaTypeToElmShallow j = case type_ j of
+  Just SchemaTypeInteger -> Right (tvar "Int")
+  Just SchemaTypeString -> Right (tvar "String")
+  Just SchemaTypeNumber -> Right (tvar "Float")
+  Just SchemaTypeBoolean -> Right (tvar "Boolean")
+  Just SchemaTypeArray -> Left "array not supported yet"
   Just SchemaTypeObject ->
     case title j of
-      Nothing -> Left "object type has no title, what do I name the type alias then?"
+      Nothing -> Left "record without a title"
+      Just title' -> Right (tvar (toPascal title'))
+  Nothing ->
+    case title j of
+      Nothing -> Left "got something without a title, don't know how to shallow-print that"
       Just title' ->
-        let alias = decTypeAlias title'
-         in Right (module_ "MyOutput" Everything [] [])
-  t -> Left ("unknown type " <> pack (show t))
+        case anyOf j of
+          Nothing -> Right (tvar (toPascal title'))
+          Just anyOfs -> do
+            subElements <- traverse schemaTypeToElmShallow anyOfs
+            pure (tparams ("Union" <> packShow (length anyOfs)) subElements)
+
+schemaObjectToDeclaration :: JsonSchemaObject -> Either Text Dec
+schemaObjectToDeclaration j = case type_ j of
+  Just SchemaTypeObject ->
+    case title j of
+      Nothing -> Left "record without a title"
+      Just title' ->
+        case properties j of
+          Nothing -> Left ("record " <> title' <> " without properties")
+          Just properties' ->
+            let requiredProps :: [Text]
+                requiredProps = fromMaybe [] (required j)
+                propToRecordField :: Text -> JsonSchemaObject -> Either Text (Text, Type)
+                propToRecordField propName obj = case schemaTypeToElmShallow (traceShowId obj) of
+                  Left e -> Left e
+                  Right v ->
+                    Right
+                      ( if propName `elem` requiredProps
+                          then (toCamel propName, v)
+                          else (toCamel propName, tparam "Maybe" v)
+                      )
+             in do
+                  recordResults <-
+                    foldM
+                      (\prevList (propName, prop) -> (: prevList) <$> propToRecordField propName prop)
+                      []
+                      (Map.toList properties')
+                  pure
+                    ( decTypeAlias
+                        title'
+                        []
+                        (trecord recordResults)
+                    )
+  Just x ->
+    case title j of
+      Nothing -> Left ("schema entry without a title " <> packShow x)
+      Just title' -> Left ("schema entry " <> title' <> " is neither object nor enum")
+  Nothing ->
+    case title j of
+      Nothing -> Left ("schema entry without a title " <> packShow j)
+      Just title' ->
+        case enum j of
+          Just enum' ->
+            let makeConstructor x = (toPascal x, [])
+             in Right (decType title' [] (makeConstructor <$> enum'))
+          Nothing -> Left ("schema entry " <> title' <> " is neither object nor enum")
+
+unionDeclaration :: Int -> Dec
+unionDeclaration n =
+  decType
+    ("Union" <> packShow n)
+    (singleton <$> take n ['a' .. 'z'])
+    (zipWith (\c i -> ("Union" <> packShow n <> "Member" <> packShow i, [tvar (singleton c)])) ['a' .. 'z'] [0 .. n])
+
+schemaToElm :: Text -> JsonSchemaObject -> Either Text Module
+schemaToElm moduleName j = case type_ j of
+  Just SchemaTypeObject -> do
+    rootDeclaration <- schemaObjectToDeclaration j
+    definitions' <- traverse schemaObjectToDeclaration (Map.elems (definitions j))
+    let unions = unionDeclaration <$> [0 .. 7]
+    pure (module_ moduleName importEvery [] (rootDeclaration : (definitions' <> unions)))
+  t -> Left ("unknown type " <> packShow t)
 
 main :: IO ()
 main = do
@@ -71,18 +129,6 @@ main = do
         Right v' -> do
           putStrLn "derefed:"
           pPrint v'
-
--- print (unionToElm (Union "TestUnion" [UnionAlternative "A1" ["Foo", "Bar"], UnionAlternative "A2" ["Qux", "Quux"]]))
-
--- case eitherDecodeStrict stdin :: Either String (SubschemaF RefOrSubschema) of
---   Left e -> putStrLn (pack e)
---   Right v -> case resolveRefs v of
---     Left e' -> putStrLn e'
---     Right resolved -> do
---       -- printSubschema resolved
---       -- putStrLn (jsonSchemaToElm Deep resolved)
---       -- case definitions (unFix resolved) of
---       --   Nothing -> pure ()
---       --   Just definitions -> putStrLn (jsonSchemaToElm Deep definitions)
---       for_ (definitions (unFix resolved)) (mapM_ (putStrLn . jsonSchemaToElm Deep))
---       putStrLn (jsonSchemaToElm Deep resolved)
+          case schemaToElm "MyModule" v' of
+            Left e -> putStrLn ("error elming: " <> e)
+            Right v'' -> putStrLn (render v'')
